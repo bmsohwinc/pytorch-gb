261e650f96cc3042d9412d342d576598068de75f
Sinclair19 <zsq0219@outlook.com>
2026-02-23 01:20:50 -0600

    fix(ddp): Wrap getCurrentCUDAStream calls with USE_CUDA macros to fix cpu build linking

--- torch/csrc/distributed/c10d/reducer.cpp
+++ torch/csrc/distributed/c10d/reducer.cpp
@@ -899,14 +899,18 @@ void Reducer::mark_variable_ready(size_t variable_index) {
   if (bucket.expect_sparse_gradient) {
     mark_variable_ready_sparse(variable_index);
   } else {
+#ifdef USE_CUDA
     if (bucket.gradients.is_cuda()) {
       c10::cuda::getCurrentCUDAStream().synchronize();
     }
+#endif
     auto start = std::chrono::steady_clock::now();
     mark_variable_ready_dense(variable_index);
+#ifdef USE_CUDA
     if (bucket.gradients.is_cuda()) {
       c10::cuda::getCurrentCUDAStream().synchronize();
     }
+#endif
     auto end = std::chrono::steady_clock::now();
     copy_times_us_.push_back(std::chrono::duration_cast<std::chrono::microseconds>(end - start).count());
   }
@@ -1258,14 +1262,18 @@ void Reducer::initialize_buckets(
       // Checking just once won't catch if someone messes with
       // param layouts over time, but not messing with params after DDP
       // construction is already a documented constraint.
+#ifdef USE_CUDA
       if (bucket.gradients.is_cuda()) {
         c10::cuda::getCurrentCUDAStream().synchronize();
       }
+#endif
       auto start = std::chrono::steady_clock::now();
       initialize_bucket_views(bucket);
+#ifdef USE_CUDA
       if (bucket.gradients.is_cuda()) {
         c10::cuda::getCurrentCUDAStream().synchronize();
       }
+#endif
       auto end = std::chrono::steady_clock::now();
       copy_times_us_.push_back(std::chrono::duration_cast<std::chrono::microseconds>(end - start).count());
     }
@@ -1781,14 +1789,18 @@ void Reducer::finalize_backward() {
       // We don't need to finalize the sparse bucket since the sparse grad and
       // the bucket essentially point to the same storage. As a result, once
       // the allreduce is done, the sparse grads are automatically updated.
+#ifdef USE_CUDA
       if (bucket.gradients.is_cuda()) {
         c10::cuda::getCurrentCUDAStream().synchronize();
       }
+#endif
       auto start = std::chrono::steady_clock::now();
       finalize_bucket_dense(bucket);
+#ifdef USE_CUDA
       if (bucket.gradients.is_cuda()) {
         c10::cuda::getCurrentCUDAStream().synchronize();
       }
+#endif
       auto end = std::chrono::steady_clock::now();
       copy_times_us_.push_back(std::chrono::duration_cast<std::chrono::microseconds>(end - start).count());
     }
